d6d7bb68faa258634d8a9902a2ef36f75eef887b
root <karim.dahdouh.fr@gmail.com>
2019-12-05 21:01:00 +0100

    atlas mongoDB conneced

--- server.js
+++ server.js
@@ -271,9 +271,8 @@ MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (err
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
 
 app.listen(port);
